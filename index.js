507e5d1a5562b299be0126d7c61bb6c81afb33a7
Alan Aliaga <alanaliagadev@gmail.com>
2018-10-26 15:45:03 -0400

    [Add] eslint, prettier config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer(function(req, res) {
-  res.end('Starting the API')
-})
+  res.end('Starting the API');
+});
 
 server.listen(8080, () => {
-  console.log('Server running in PORT: ', 8080)
-})
+  console.log('Server running in PORT: ', 8080);
+});
