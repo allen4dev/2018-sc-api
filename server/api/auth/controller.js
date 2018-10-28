a48c060e81c30cd49fbb69c7ec8f53b0d47ec6f6
allen4dev <alanaliagadev@gmail.com>
2018-10-27 23:52:06 -0400

    [Update] minor updates

--- server/api/auth/controller.js
+++ server/api/auth/controller.js
@@ -8,6 +8,17 @@ exports.create = function signup(req, res, next) {
 
   Artist.register(new Artist({ email, name, tags }), password)
     .then(res => jwt.sign({ _id: res._id }, 'secret'))
-    .then(token => res.status(201).json({ data: { attributes: { token } } }))
+    .then(token =>
+      res.status(201).json({
+        data: { attributes: { token } },
+      }),
+    )
     .catch(next);
+};
+
+exports.show = function signin(req, res, next) {
+  const { _id } = req.user;
+
+  const token = jwt.sign({ _id }, 'secret');
+  res.json({ data: { attributes: { token, _id } } });
 };
--- server/api/auth/router.js
+++ server/api/auth/router.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const passport = require('passport');
 
 const controller = require('./controller');
 
 const router = Router();
 
-router.use('/register', controller.create);
+router.post('/register', controller.create);
+router.post('/login', passport.authenticate('local'), controller.show);
 
 module.exports = router;
