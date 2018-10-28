93f8debcf1a9f73eed900ed96244d7597ce8a119
Alan Aliaga <alanaliagadev@gmail.com>
2018-10-27 20:54:21 -0400

    [Update | ToDo] sign a jwt after create | refactor

--- server/api/auth/controller.js
+++ server/api/auth/controller.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const Artist = mongoose.model('Artist');
 
 exports.create = function signup(req, res, next) {
   const { email, password, name, tags = [] } = req.body.data;
 
-  Artist.register(new Artist({ email, name, tags }), password, err => {
-    if (err) return next(err);
-
-    res.status(201).json({ data: { attributes: { email, name } } });
-  });
+  Artist.register(new Artist({ email, name, tags }), password)
+    .then(res => jwt.sign({ _id: res._id }, 'secret'))
+    .then(token => res.status(201).json({ data: { attributes: { token } } }))
+    .catch(next);
 };
